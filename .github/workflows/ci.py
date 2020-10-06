bcbb7dd50b638d5df2ed9755b3eac82526e1855c
Robbie Harwood <rharwood@redhat.com>
2020-10-06 17:25:13 -0400

    [CI] Style cleanup

--- .github/workflows/ci.py
+++ .github/workflows/ci.py
@@ -1,50 +1,45 @@
 #!/usr/bin/python3
 
-import shlex
 import subprocess
-import sys
 
 from typing import Dict, Optional
 
-def p(s: str) -> None:
-    sys.stdout.write(f"{s}\n")
-    sys.stdout.flush()
-
-def r(c: str, e: Optional[Dict[str, str]] = None, fail: bool = False) -> None:
+def run(c: str, env: Optional[Dict[str, str]] = None,
+        fail: bool = False) -> None:
     proc = subprocess.run(c, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                          shell=True, input=b"hi", env=e)
+                          shell=True, input=b"hi", env=env)
     if proc.returncode == 0 and not fail:
-        p(f"[expected pass] `{c}` with env {e}")
+        print(f"[expected pass] `{c}` with env {e}")
     elif proc.returncode != 0 and fail:
-        p(f"[expected fail] `{c}` with env {e}")
+        print(f"[expected fail] `{c}` with env {e}")
     elif proc.returncode == 0 and fail:
-        p(f"[unexpected pass] `{c}` with env {e}")
-        p(f"Output:\n{proc.stdout.decode('utf-8')}")
+        print(f"[unexpected pass] `{c}` with env {e}")
+        print(f"Output:\n{proc.stdout.decode('utf-8')}")
         exit(-1)
     else:
-        p(f"[unexpected fail] `{c}` with env {e}")
-        p(f"Output:\n{proc.stdout.decode('utf-8')}")
+        print(f"[unexpected fail] `{c}` with env {e}")
+        print(f"Output:\n{proc.stdout.decode('utf-8')}")
         exit(-1)
 
-r("fips-finish-install --complete")
+run("fips-finish-install --complete")
 
-env = {"LD_PRELOAD": f"./forcemd5.so"}
+env = {"LD_PRELOAD": "./forcemd5.so"}
 
-p("Sanity checks first:")
-r("openssl md5", env)
-r("openssl md4", env)
+print("Sanity checks first:")
+run("openssl md5", env)
+run("openssl md4", env)
 
-p("These are both forbidden under FIPS:")
+print("These are both forbidden under FIPS:")
 env["OPENSSL_FORCE_FIPS_MODE"] = "1"
-r("openssl md5", env, fail=True)
-r("openssl md4", env, fail=True)
+run("openssl md5", env, fail=True)
+run("openssl md4", env, fail=True)
 
-p("The env var should only control MD5:")
+print("The env var should only control MD5:")
 env["OPENSSL_FIPS_NON_MD5_ALLOW"] = "1"
-r("openssl md5", env)
-r("openssl md4", env, fail=True)
+run("openssl md5", env)
+run("openssl md4", env, fail=True)
 
-p("The env var shouldn't break non-FIPS runs:")
+print("The env var shouldn't break non-FIPS runs:")
 del(env["OPENSSL_FORCE_FIPS_MODE"])
-r("openssl md4", env)
-r("openssl md5", env)
+run("openssl md4", env)
+run("openssl md5", env)
